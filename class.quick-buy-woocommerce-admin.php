db9860c99250cbd5f6f5177298ac85c77ff46baa
Vu Ngoc Giao <giaovn49@gmail.com>
2019-07-04 22:50:08 +0700

    update code

---
+++ class.quick-buy-woocommerce-admin.php
@@ -0,0 +1,14 @@
+<?php
+class quick_buy_admin{
+	protected static $instance;
+	public function __construct(){
+
+	}
+	public static function get_instance(){
+
+	}
+	public static function run(){
+
+	}
+}
+?>
