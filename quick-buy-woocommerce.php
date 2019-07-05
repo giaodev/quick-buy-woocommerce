df47164c9706d847c94becbc84f16abe98f52758
Vu Ngoc Giao <giaovn49@gmail.com>
2019-07-05 10:27:28 +0700

    update code, class, move code..

--- quick-buy-woocommerce.php
+++ quick-buy-woocommerce.php
@@ -18,6 +18,8 @@ define( 'QBW_VERSION', '0.1.0' );
 define( 'QBW_MINIMUM_WP_VERSION', '4.0' );
 define( 'QBW_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'QBW_DELETE_LIMIT', 100000 );
+require_once( QBW_PLUGIN_DIR . 'class.quick-buy-woocommerce-admin.php' );
+require_once( QBW_PLUGIN_DIR . 'class.quick-buy-woocommerce-views.php' );
 require_once( QBW_PLUGIN_DIR . 'class.quick-buy-woocommerce.php' );
 quick_buy_woocommerce::run();
 register_activation_hook( __FILE__, array( 'quick_buy_woocommerce', 'plugin_activation' ) );
