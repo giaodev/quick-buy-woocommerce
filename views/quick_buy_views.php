541f3a238d700ae27d5cd92164e12930959b3007
Vu Ngoc Giao <giaovn49@gmail.com>
2019-07-05 10:51:26 +0700

    update css, placheholder cho input

--- views/quick_buy_views.php
+++ views/quick_buy_views.php
@@ -12,13 +12,13 @@
     <div class="modal-body">
       <form action="" method="post">
         <label for="">Tên</label>
-        <input type="text" class="input-text " name="name" id="name" placeholder="" value="">
+        <input type="text" class="input-text " name="name" id="name" placeholder="Tên" value="">
         <br>
         <label for="">Địa chỉ</label>
-        <input type="text" class="input-text " name="address" id="address" placeholder="Địa chỉ" value=""placeholder="Địa chỉ">
+        <input type="text" class="input-text " name="address" id="address" placeholder="Địa chỉ" value="" placeholder="Địa chỉ">
         <br>
         <label for="">Số điện thoại</label>
-        <input type="tel" class="input-text " name="phone" id="phone" placeholder="" value="">
+        <input type="tel" class="input-text " name="phone" id="phone" placeholder="Số điện thoại" value="">
         <br>
         <label for="">Ghi chú</label>
         <textarea name="note" class="input-text " id="note" placeholder="Ghi chú về đơn hàng, ví dụ: thời gian hay chỉ dẫn địa điểm giao hàng chi tiết hơn." rows="2" cols="5"></textarea>
